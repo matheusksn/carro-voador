6b87ae8577af3ccd5bf7b0145c4171143070094c
Matheus <101477339+matheusksn@users.noreply.github.com>
2023-02-13 21:48:14 -0300

    Arquivos da primeira aula

---
+++ src/main/java/com/impacta/carrovoador/CarroVoadorApplication.java
@@ -0,0 +1,13 @@
+package com.impacta.carrovoador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroVoadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroVoadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impacta/carrovoador/CarroVoadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impacta.carrovoador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroVoadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
